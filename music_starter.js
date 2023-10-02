ef0d85afc14e0719b8638f32250fbdb8343af6ea
baolong2005 <baolongvt2005@gmail.com>
2023-10-03 04:26:06 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -4,24 +4,36 @@
   let firstRun = true;
   let testImg;
 
+
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  colorMode(HSB, 100);
-  background(20)
-  textFont('Helvetica'); // please use CSS safe fonts
-  rectMode(CENTER)
+  background(20)  
   textSize(24);
-  strokeWeight(1);
-  
+
+    
   let vocalMap = map(vocal, 0, 100, 60, 100);
   let posXofCircle = 480;
   let posYofCircle = 270;
+  let blackColor = color(5, 5, 5);
+  let grayColor = color(51, 51, 51);
+    console.log(song.currentTime());
 
-  stroke(vocalMap, 100, 100);
+    noFill();
+    strokeWeight(3);
+  
+  let sizeofBlock = 600;
+  for(let i = 0; i < sizeofBlock; i++){
+  let gradientAmount = map(i, 0, sizeofBlock, 0, 1);
+  let strokeColor = lerpColor(blackColor, grayColor, gradientAmount);
+    stroke(strokeColor);
+    line(0, i, width, i);
+  }
+    colorMode(HSB, 100);
+    stroke(vocalMap, 100, 100);
+    strokeWeight(1);
   for(let i = 1; i <= vocalMap; i++){
-    let sizeofCircle = i*3;
+  let sizeofCircle = i*3;
     fill(20);
     ellipse(posXofCircle, posYofCircle, sizeofCircle);
-    ellipse(posXofCircle, posYofCircle, sizeofCircle-10);
   }
   if(firstRun){
     
@@ -31,9 +43,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   }
     scale(0.4);
     image(testImg, 944, 419);
-    
-   // display "words"
-   //textAlign(CENTER);
-   //textSize(vocal);
-   //text(words, width/2, height/3);
+
+
+
 }
