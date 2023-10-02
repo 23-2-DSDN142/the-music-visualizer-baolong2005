cf1a8e7105cf99696cc052dfbd76e9b6ed5edb25
baolong2005 <baolongvt2005@gmail.com>
2023-10-02 16:42:17 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -5,39 +5,37 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   textFont('Helvetica'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
+  noStroke();
 
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
+   let vocalColor = vocal;
+   let purpleColor = color(89, 1, 105);
+   let pinkColor = color(217, 2, 156);
+   let sizeofBlock = 100;
+   for(let i = 0; i < width; i++){
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
+   let gradientAmount = map(i, 0, sizeofBlock, 0, 1);
+   let strokeColor = lerpColor(vocalColor, purpleColor, pinkColor, gradientAmount);
+    
+  
+    stroke(strokeColor);
+    ellipse(width/2, height/2, i);
+    line(0, 100+i, width, 100+i);
+   }
+    stroke(purpleColor);
+
+   let bassWave = map(bass, 0, height, 0, 1);
+   let waveHeight = bassWave;
+   let waveFreq = 8;
+
+   let bassYLoki = height/2;
+    beginShape();
+   for(let i = 0; i < width; i++){
+    vertex(i, bassYLoki - waveHeight*sin(waveFreq*2));
+    }
+    endShape();
  
    // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+   //textAlign(CENTER);
+   //textSize(vocal);
+   //text(words, width/2, height/3);
 }
