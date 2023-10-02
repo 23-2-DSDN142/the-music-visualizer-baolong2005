7f08b54a839bd124fe65ec65d572566bf01fe43d
baolong2005 <baolongvt2005@gmail.com>
2023-10-03 01:02:34 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,8 +1,8 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
   
-  //let firstRun = true;
-  //let testImg;
+  let firstRun = true;
+  let testImg;
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   colorMode(HSB, 100);
@@ -11,24 +11,26 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   rectMode(CENTER)
   textSize(24);
   strokeWeight(1);
-  //if(firstRun){
-    //testImg = loadImage('football_image');
-
-    //firstRun = false;
-  //}
-
-    //image(testImg, 500, 50);
-  let vocalMap = map(vocal, 0, 100, 40, 80);
+  
+  let vocalMap = map(vocal, 0, 100, 60, 100);
   let posXofCircle = 480;
   let posYofCircle = 270;
 
   stroke(vocalMap, 100, 100);
   for(let i = 1; i <= vocalMap; i++){
-    let sizeofCircle = i*5;
+    let sizeofCircle = i*3;
     fill(20);
     ellipse(posXofCircle, posYofCircle, sizeofCircle);
     ellipse(posXofCircle, posYofCircle, sizeofCircle-10);
   }
+  if(firstRun){
+    
+    testImg = loadImage('football_image.png');
+
+    firstRun = false
+  }
+    scale(0.4);
+    image(testImg, 944, 419);
     
    // display "words"
    //textAlign(CENTER);
