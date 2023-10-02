895a3a39a103737c4024eeddf0f40ec4c286eb69
baolong2005 <baolongvt2005@gmail.com>
2023-10-02 17:18:04 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -5,35 +5,20 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   textFont('Helvetica'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
-  noStroke();
+  strokeWeight(9);
+  stroke(drum, 80, 80);
 
-   let vocalColor = vocal;
-   let purpleColor = color(89, 1, 105);
-   let pinkColor = color(217, 2, 156);
-   let sizeofBlock = 100;
-   for(let i = 0; i < width; i++){
+  let drumMap = map(drum, 0, 100, 30, 90);
+  let lengthOfLine = 300;
+  let LineStart = 100
+  let LineEnd = LineStart + lengthOfLine;
 
-   let gradientAmount = map(i, 0, sizeofBlock, 0, 1);
-   let strokeColor = lerpColor(vocalColor, purpleColor, pinkColor, gradientAmount);
-    
   
-    stroke(strokeColor);
-    ellipse(width/2, height/2, i);
-    line(0, 100+i, width, 100+i);
-   }
-    stroke(purpleColor);
-
-   let bassWave = map(bass, 0, height, 0, 1);
-   let waveHeight = bassWave;
-   let waveFreq = 8;
-
-   let bassYLoki = height/2;
-    beginShape();
-   for(let i = 0; i < width; i++){
-    vertex(i, bassYLoki - waveHeight*sin(waveFreq*2));
-    }
-    endShape();
- 
+  for(let i = 1; i < 5; i++){
+    let lineStep = i*10;
+    line(LineStart, lineStep, LineEnd, lineStep);
+  }
+   
    // display "words"
    //textAlign(CENTER);
    //textSize(vocal);
