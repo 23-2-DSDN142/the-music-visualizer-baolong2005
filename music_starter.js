6772f39606ddcb37e8989ef9cecc19e33c46f600
baolong2005 <baolongvt2005@gmail.com>
2023-10-05 19:49:09 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -12,7 +12,8 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   textSize(24);
   stroke(255);
   angleMode(DEGREES);
-
+  colorMode(RGB);
+  let vocalMap = map(drum, 0, 100, 60, 90);
   let bassMap = map(bass, 0, 100, 50, 150);
   let blackColor = color(5, 5, 5);
   let grayColor = color(51, 51, 51);
@@ -41,7 +42,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     beginShape()
   for(let i = 0; i <= 180; i++){
   let index = floor(map(i, 0, 180, 0, wave.length - 950));
-  let r = map(wave[index], -1, 1, 100, 205)
+  let r = map(wave[index], -1, 1, 150, 205)
   let x = r * sin(i);
   let y = r * cos(i); 
     vertex(x, y);
@@ -50,7 +51,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     beginShape()
   for(let i = 0; i <= 180; i++){
   let index = floor(map(i, 0, 180, 0, wave.length - 950));
-  let r = map(wave[index], -1, 1, 100, 205)
+  let r = map(wave[index], -1, 1, 150, 205)
   let x = r * -sin(i);
   let y = r * cos(i); 
       vertex(x, y);
@@ -59,7 +60,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   beginShape()
   for(let i = 0; i <= 180; i++){
   let index = floor(map(i, 0, 180, 0, wave.length - 1000));
-  let r = map(wave[index], -1, 1, 100, 200)
+  let r = map(wave[index], -1, 1, 150, 200)
   let x = r * sin(i);
   let y = r * cos(i); 
     vertex(x, y);
@@ -68,11 +69,34 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     beginShape()
   for(let i = 0; i <= 180; i++){
   let index = floor(map(i, 0, 180, 0, wave.length - 1000));
-  let r = map(wave[index], -1, 1, 100, 200)
+  let r = map(wave[index], -1, 1, 150, 200)
   let x = r * -sin(i);
   let y = r * cos(i); 
       vertex(x, y);
       endShape()
+  }
+  
+
+angleMode(RADIANS)
+  //translate(width * 0.8, height * 0.5);
+  scale(vocalMap / 40)
+  rotate(frameCount / -100.0);
+  star(0, 0, 30, 70, 5);
+  
+
+  function star(x, y, radius1, radius2, npoints) {
+    let angle = TWO_PI / npoints;
+    let halfAngle = angle / 2.0;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius2;
+      let sy = y + sin(a) * radius2;
+      vertex(sx, sy);
+      sx = x + cos(a + halfAngle) * radius1;
+      sy = y + sin(a + halfAngle) * radius1;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
   }
     
   //if(firstRun){
